c1b635cc7f2fbbd3b17693b3ead7fbb97c9b1cc1
Tarig Ali <s2511709@ed.ac.uk>
2024-03-01 03:31:29 +0000

    MVP Baby

--- Flask1/asdmmurder.py
+++ Flask1/asdmmurder.py
@@ -17,29 +17,40 @@ def fetch_enter_new_area():
     c = conn.cursor()
 
     # Execute your query
-    c.execute("""SELECT SDO_UTIL.TO_GEOJSON(g.location) AS geojson, e.name, e.type, e.Details
-FROM GAME_ELEMENTS e
-JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
-WHERE e.geometry_id IN (
-      SELECT e.geometry_id
-      FROM GAME_ELEMENTS e
-      JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
-      WHERE SDO_CONTAINS(
-            (SELECT location FROM GEOMETRIES WHERE geometry_id = (
-                 SELECT g.geometry_id
-                 FROM GAME_ELEMENTS e
-                 JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
-                 WHERE e.type = 'Area'
-                 AND SDO_CONTAINS(g.location, (SELECT current_location FROM PLAYERS WHERE player_id = 2)) = 'TRUE'
-            )),
-            g.location) = 'TRUE'
-      UNION
-      SELECT g.geometry_id
-      FROM GAME_ELEMENTS e
-      JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
-      WHERE e.type = 'Area'
-      AND SDO_CONTAINS(g.location, (SELECT current_location FROM PLAYERS WHERE player_id = 2)) = 'TRUE'
-)
+    c.execute("""
+    SELECT 
+        SDO_UTIL.TO_GEOJSON(g.location) AS geojson, 
+        e.name, 
+        e.type, 
+        e.details
+    FROM 
+        GAME_ELEMENTS e
+    JOIN 
+        GEOMETRIES g ON e.geometry_id = g.geometry_id
+    WHERE 
+        e.geometry_id IN (
+            SELECT e.geometry_id
+            FROM GAME_ELEMENTS e
+            JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
+            WHERE SDO_CONTAINS(
+                (SELECT location FROM GEOMETRIES WHERE geometry_id = (
+                    SELECT g.geometry_id
+                    FROM GAME_ELEMENTS e
+                    JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
+                    WHERE e.type = 'Area'
+                    AND SDO_CONTAINS(g.location, (SELECT current_location FROM PLAYERS WHERE player_id = 2)) = 'TRUE'
+                )),
+                g.location) = 'TRUE'
+            UNION
+            SELECT g.geometry_id
+            FROM GAME_ELEMENTS e
+            JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
+            WHERE e.type = 'Area'
+            AND SDO_CONTAINS(g.location, (SELECT current_location FROM PLAYERS WHERE player_id = 2)) = 'TRUE'
+        )
+    AND e.type IN ('Clue', 'Suspect') 
+    AND g.geometry_id NOT IN (13, 14, 12)
+
 """)
 
 
@@ -276,8 +287,8 @@ def polygon_area():
     return Response(json.dumps(geojson_data), mimetype='application/json')
 ## Polygon area
 
-## Game Elements within 300m
-def fetch_300m_search():
+## Game Elements within 200m
+def fetch_200m_search():
     conn = cx_Oracle.connect(dsn=app.config['DATABASE_DSN'],
                              user=app.config['DATABASE_USER'],
                              password=app.config['DATABASE_PASSWORD'])
@@ -291,7 +302,7 @@ def fetch_300m_search():
             WHERE SDO_WITHIN_DISTANCE(
             g.location, 
             (SELECT current_location FROM PLAYERS WHERE player_id = 2), 
-            'distance=300 unit=METER') = 'TRUE'
+            'distance=200 unit=METER') = 'TRUE'
     """)
 
     # Construct GeoJSON features including the DESCRIPTION
@@ -319,11 +330,56 @@ def fetch_300m_search():
     conn.close()
 
     return geojson_collection
-@app.route('/300m_search')
-def search_300m():
-    geojson_data = fetch_300m_search()
+@app.route('/200m_search')
+def search_200m():
+    geojson_data = fetch_200m_search()
     return Response(json.dumps(geojson_data), mimetype='application/json')
-## Game Elements Within 300m 
+## Game Elements Within 200m 
+
+## Buffer Suspect 400m
+def fetch_400m_susbuf():
+    conn = cx_Oracle.connect(dsn=app.config['DATABASE_DSN'],
+                             user=app.config['DATABASE_USER'],
+                             password=app.config['DATABASE_PASSWORD'])
+    cursor = conn.cursor()
+
+    # Execute your query
+    cursor.execute(""" 
+        SELECT SDO_UTIL.TO_GEOJSON(SDO_GEOM.SDO_BUFFER(g.location, 400, 0.005)) AS geojson, e.name, e.type
+                FROM GAME_ELEMENTS e
+                JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
+               WHERE e.type = 'Suspect'
+    """)
+
+    # Construct GeoJSON features including the DESCRIPTION
+    geojson_features = []
+    for row in cursor:
+        geojson, name, type = row
+        feature = {
+            'type': 'Feature',
+            'geometry': json.loads(geojson.read() if geojson else '{}'),
+            'properties': {
+                'Name': name,
+                'Type': type,
+            }
+        }
+        geojson_features.append(feature)
+
+    geojson_collection = {
+        'type': 'FeatureCollection',
+        'features': geojson_features
+    }
+
+    cursor.close()
+    conn.close()
+
+    return geojson_collection
+@app.route('/400m_susbuf')
+def susbuf_200m():
+    geojson_data = fetch_400m_susbuf()
+    return Response(json.dumps(geojson_data), mimetype='application/json')
+# Buffer Suspect 400m
+
 
 ## Buffer Suspect 40m
 def fetch_40m_susbuf():
@@ -334,16 +390,131 @@ def fetch_40m_susbuf():
 
     # Execute your query
     cursor.execute(""" 
-        SELECT SDO_UTIL.TO_GEOJSON(SDO_GEOM.SDO_BUFFER(g.location, 40, 0.005)) AS geojson, e.name, e.type, e.details
-                FROM GAME_ELEMENTS e
-                JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
-               WHERE e.type = 'Suspect'
+        SELECT SDO_UTIL.TO_GEOJSON(SDO_GEOM.SDO_BUFFER(g.location, 40, 0.005)) AS buffered_geojson, e.name, e.type
+        FROM GAME_ELEMENTS e
+        JOIN GEOMETRIES g ON e.geometry_id = g.geometry_id
+        WHERE e.type = 'Suspect'
+        AND SDO_WITHIN_DISTANCE(g.location, 
+            (SELECT p.current_location 
+            FROM PLAYERS p
+            WHERE p.player_id = 2), 'distance = 100') = 'TRUE'
+        AND EXISTS (
+        SELECT 1
+        FROM GAME_ELEMENTS area
+        JOIN GEOMETRIES area_g ON area.geometry_id = area_g.geometry_id
+        WHERE area.type IN ('Area', 'Solution Area')
+        AND SDO_CONTAINS(area_g.location, 
+            (SELECT p.current_location
+                FROM PLAYERS p
+                WHERE p.player_id = 2)) = 'TRUE'
+        AND SDO_CONTAINS(area_g.location, g.location) = 'TRUE'
+    )
     """)
 
     # Construct GeoJSON features including the DESCRIPTION
     geojson_features = []
     for row in cursor:
-        geojson, name, type, details = row
+        buffered_geojson, name, type = row
+        feature = {
+            'type': 'Feature',
+            'geometry': json.loads(buffered_geojson.read() if buffered_geojson else '{}'),
+            'properties': {
+                'Name': name,
+                'Type': type,
+            }
+        }
+        geojson_features.append(feature)
+
+    geojson_collection = {
+        'type': 'FeatureCollection',
+        'features': geojson_features
+    }
+
+    cursor.close()
+    conn.close()
+
+    return geojson_collection
+@app.route('/40m_susbuf')
+def susbuf_40m():
+    geojson_data = fetch_40m_susbuf()
+    return Response(json.dumps(geojson_data), mimetype='application/json')
+# Buffer Suspect 40m
+
+## Nearest Point
+def fetch_nearest_point():
+    conn = cx_Oracle.connect(dsn=app.config['DATABASE_DSN'],
+                             user=app.config['DATABASE_USER'],
+                             password=app.config['DATABASE_PASSWORD'])
+    cursor = conn.cursor()
+
+    # Execute your query
+    cursor.execute(""" 
+    SELECT  SDO_UTIL.TO_GEOJSON(g.location) AS geojson, e.name, e.type, e.details, 
+                   ROUND(SDO_GEOM.SDO_DISTANCE(g.location, p.current_location, 0.005), 2) AS distance 
+                   FROM  GAME_ELEMENTS e JOIN  GEOMETRIES g ON e.geometry_id = g.geometry_id JOIN  PLAYERS p ON p.player_id = 2 
+                   WHERE  e.type IN ('Clue', 'Suspect', 'Item') ORDER BY  distance ASC FETCH FIRST 1 ROW ONLY
+    """)
+
+    # Construct GeoJSON features including the DESCRIPTION
+    geojson_features = []
+    for row in cursor:
+        geojson, name, type, details, distance = row
+        feature = {
+            'type': 'Feature',
+            'geometry': json.loads(geojson.read() if geojson else '{}'),
+            'properties': {
+                'Name': name,
+                'Type': type,
+                'Details': details,
+                'Distance': distance
+            }
+        }
+        geojson_features.append(feature)
+
+    geojson_collection = {
+        'type': 'FeatureCollection',
+        'features': geojson_features
+    }
+
+    cursor.close()
+    conn.close()
+
+    return geojson_collection
+@app.route('/nearest_point')
+def nearest_point():
+    geojson_data = fetch_nearest_point()
+    return Response(json.dumps(geojson_data), mimetype='application/json')
+# Nearest Point
+
+
+
+## Puzzle 2 solution 
+def fetch_puzzle2_solution():
+    conn = cx_Oracle.connect(dsn=app.config['DATABASE_DSN'],
+                             user=app.config['DATABASE_USER'],
+                             password=app.config['DATABASE_PASSWORD'])
+    cursor = conn.cursor()
+
+    # Execute your query
+    cursor.execute(""" 
+SELECT 
+        SDO_UTIL.TO_GEOJSON(g.location) AS geojson,
+        e.name, 
+        e.type, 
+        e.details
+        
+        FROM 
+            GAME_ELEMENTS e
+        JOIN 
+            GEOMETRIES g ON e.geometry_id = g.geometry_id
+        WHERE 
+        e.element_id = 7
+    """)
+
+    # Construct GeoJSON features including the DESCRIPTION
+    geojson_features = []
+    for row in cursor:
+        geojson, name, type, details= row
         feature = {
             'type': 'Feature',
             'geometry': json.loads(geojson.read() if geojson else '{}'),
@@ -364,11 +535,278 @@ def fetch_40m_susbuf():
     conn.close()
 
     return geojson_collection
-@app.route('/40m_susbuf')
-def susbuf_40m():
-    geojson_data = fetch_40m_susbuf()
+@app.route('/puzzle2_solution')
+def puzle2_solution():
+    geojson_data = fetch_puzzle2_solution()
     return Response(json.dumps(geojson_data), mimetype='application/json')
-# Buffer Suspect 40m
+#  Puzzle 2 Solution
+
+## Puzzle 3 solution 
+def fetch_puzzle3_solution():
+    conn = cx_Oracle.connect(dsn=app.config['DATABASE_DSN'],
+                             user=app.config['DATABASE_USER'],
+                             password=app.config['DATABASE_PASSWORD'])
+    cursor = conn.cursor()
+
+    # Execute your query
+    cursor.execute(""" 
+SELECT 
+        SDO_UTIL.TO_GEOJSON(g.location) AS geojson,
+        e.name, 
+        e.type, 
+        e.details
+        
+        FROM 
+            GAME_ELEMENTS e
+        JOIN 
+            GEOMETRIES g ON e.geometry_id = g.geometry_id
+        WHERE 
+        e.element_id = 12
+    """)
+
+    # Construct GeoJSON features including the DESCRIPTION
+    geojson_features = []
+    for row in cursor:
+        geojson, name, type, details= row
+        feature = {
+            'type': 'Feature',
+            'geometry': json.loads(geojson.read() if geojson else '{}'),
+            'properties': {
+                'Name': name,
+                'Type': type,
+                'Details': details
+            }
+        }
+        geojson_features.append(feature)
+
+    geojson_collection = {
+        'type': 'FeatureCollection',
+        'features': geojson_features
+    }
+
+    cursor.close()
+    conn.close()
+
+    return geojson_collection
+@app.route('/puzzle3_solution')
+def puzle3_solution():
+    geojson_data = fetch_puzzle3_solution()
+    return Response(json.dumps(geojson_data), mimetype='application/json')
+#  Puzzle 3 Solution
+
+## Enter Polygon 4
+def fetch_enter_polygon4():
+    conn = cx_Oracle.connect(dsn=app.config['DATABASE_DSN'],
+                             user=app.config['DATABASE_USER'],
+                             password=app.config['DATABASE_PASSWORD'])
+    cursor = conn.cursor()
+
+    # Execute your query
+    cursor.execute(""" 
+SELECT 
+    SDO_UTIL.TO_GEOJSON(g.location) AS geojson, 
+    e.name, 
+    e.type, 
+    e.details
+FROM 
+    GAME_ELEMENTS e
+JOIN 
+    GEOMETRIES g ON e.geometry_id = g.geometry_id
+WHERE 
+    g.geometry_id = 24 AND e.type = 'Area'
+UNION ALL
+SELECT 
+    SDO_UTIL.TO_GEOJSON(g.location) AS geojson, 
+    e.name, 
+    e.type, 
+    e.details
+FROM 
+    GAME_ELEMENTS e
+JOIN 
+    GEOMETRIES g ON e.geometry_id = g.geometry_id
+JOIN 
+    PLAYERS p ON p.player_id = 2
+WHERE 
+    SDO_CONTAINS(
+        (SELECT location FROM GEOMETRIES WHERE geometry_id = 24), 
+        g.location
+    ) = 'TRUE'
+    AND e.type IN ('Clue', 'Suspect', 'Item')
+    AND EXISTS (
+        SELECT 1 
+        FROM PLAYERS p
+        WHERE p.player_id = 2
+        AND SDO_CONTAINS(
+            (SELECT location FROM GEOMETRIES WHERE geometry_id = 19), 
+            p.current_location
+        ) = 'TRUE'
+        AND SDO_WITHIN_DISTANCE(
+            p.current_location, 
+            (SELECT location FROM GEOMETRIES WHERE geometry_id = 24), 
+            'distance=300 unit=METER'
+        ) = 'TRUE')
+
+    """)
+
+    # Construct GeoJSON features including the DESCRIPTION
+    geojson_features = []
+    for row in cursor:
+        geojson, name, type, details= row
+        feature = {
+            'type': 'Feature',
+            'geometry': json.loads(geojson.read() if geojson else '{}'),
+            'properties': {
+                'Name': name,
+                'Type': type,
+                'Details': details
+            }
+        }
+        geojson_features.append(feature)
+
+    geojson_collection = {
+        'type': 'FeatureCollection',
+        'features': geojson_features
+    }
+
+    cursor.close()
+    conn.close()
+
+    return geojson_collection
+@app.route('/enter_polygon4')
+def enter_polygon4():
+    geojson_data = fetch_enter_polygon4()
+    return Response(json.dumps(geojson_data), mimetype='application/json')
+#  Enter Polygon 4
+
+
+## Find Item 100m
+def fetch_find_item100m():
+    conn = cx_Oracle.connect(dsn=app.config['DATABASE_DSN'],
+                             user=app.config['DATABASE_USER'],
+                             password=app.config['DATABASE_PASSWORD'])
+    cursor = conn.cursor()
+
+    # Execute your query
+    cursor.execute(""" 
+    SELECT 
+        SDO_UTIL.TO_GEOJSON(g.location) AS geojson,           
+        e.element_id AS item_id, 
+        e.name AS item_name, 
+        e.type AS item_type, 
+        e.details AS item_details
+    FROM 
+        GAME_ELEMENTS e
+    JOIN 
+        GEOMETRIES g ON e.geometry_id = g.geometry_id,
+        (SELECT current_location FROM PLAYERS WHERE player_id = 2) p
+    WHERE 
+        e.type = 'Item'
+        AND SDO_WITHIN_DISTANCE(g.location, p.current_location, 'distance=100 unit=METER') = 'TRUE'
+        AND EXISTS (
+            SELECT 1
+            FROM GAME_ELEMENTS e2
+            JOIN GEOMETRIES g2 ON e2.geometry_id = g2.geometry_id
+            WHERE (e2.type = 'Area' OR e2.type = 'Solution Area')
+            AND SDO_CONTAINS(g2.location, g.location) = 'TRUE'
+            AND SDO_CONTAINS(g2.location, p.current_location) = 'TRUE'
+    )
+
+    """)
+
+    # Construct GeoJSON features including the DESCRIPTION
+    geojson_features = []
+    for row in cursor:
+        geojson, item_id, name, type, item_details= row
+        feature = {
+            'type': 'Feature',
+            'geometry': json.loads(geojson.read() if geojson else '{}'),
+            'properties': {
+                'Item ID': item_id,
+                'Name': name,
+                'Type': type,
+                'Details': item_details
+            }
+        }
+        geojson_features.append(feature)
+
+    geojson_collection = {
+        'type': 'FeatureCollection',
+        'features': geojson_features
+    }
+
+    cursor.close()
+    conn.close()
+
+    return geojson_collection
+@app.route('/find_item100m')
+def find_item100m():
+    geojson_data = fetch_find_item100m()
+    return Response(json.dumps(geojson_data), mimetype='application/json')
+#  Find Item 100m
+
+
+
+## Find the culprit
+def fetch_the_culprit():
+    conn = cx_Oracle.connect(dsn=app.config['DATABASE_DSN'],
+                             user=app.config['DATABASE_USER'],
+                             password=app.config['DATABASE_PASSWORD'])
+    cursor = conn.cursor()
+
+    # Execute your query
+    cursor.execute(""" 
+        SELECT 
+        SDO_UTIL.TO_GEOJSON(SDO_GEOM.SDO_BUFFER(g.location, 1000, 0.005)) AS buffered_geojson,
+        e.element_id, 
+        e.type
+        
+    FROM 
+        GAME_ELEMENTS e
+    JOIN 
+        GEOMETRIES g ON e.geometry_id = g.geometry_id
+    WHERE 
+        e.type = 'Item'
+        AND NOT EXISTS (
+            SELECT 1
+            FROM GAME_ELEMENTS polygons
+            JOIN GEOMETRIES polyGeom ON polygons.geometry_id = polyGeom.geometry_id
+            WHERE polygons.type IN ('Area', 'Solution Area')
+            AND SDO_CONTAINS(polyGeom.location, g.location) = 'TRUE'
+        )
+
+    """)
+
+    # Construct GeoJSON features including the DESCRIPTION
+    geojson_features = []
+    for row in cursor:
+        buffered_geojson, element_id, type = row
+        feature = {
+            'type': 'Feature',
+            'geometry': json.loads(buffered_geojson.read() if buffered_geojson else '{}'),
+            'properties': {
+                'Item ID': element_id,
+                'Type': type,
+            }
+        }
+        geojson_features.append(feature)
+
+    geojson_collection = {
+        'type': 'FeatureCollection',
+        'features': geojson_features
+    }
+
+    cursor.close()
+    conn.close()
+
+    return geojson_collection
+@app.route('/find_culprit')
+def find_culprit():
+    geojson_data = fetch_the_culprit()
+    return Response(json.dumps(geojson_data), mimetype='application/json')
+#  Find the culprit
+
+
+
 
 ## Update location
 @app.route('/update-location', methods=['POST'])
